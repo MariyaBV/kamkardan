018b2f2f9da1982044c3c3fa206b38bfd1732488
Yun Andrey <mrsashok86@gmail.com>
2024-09-04 23:48:05 +0200

    delivery

---
+++ plugins/delivery-mne/delivery-mne.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Delivery_MNE
+ * Description: Расчет доставки.
+ * Version: 1.0.0
+ * Text Domain:       delivery-mne
+ * Domain Path: /languages
+ */
+
+// Проверяем, что файл не вызывается напрямую
+if (!defined('ABSPATH')) {
+    exit; // Завершаем выполнение скрипта
+}
+
+function mne_check_woocommerce()
+{
+    // Проверяем, активен ли плагин WooCommerce
+    if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+        // WooCommerce не активен, выводим сообщение и деактивируем плагин
+        add_action('admin_notices', 'mne_woocommerce_not_active_notice');
+        deactivate_plugins(plugin_basename(__FILE__));
+    }
+}
+
+function mne_woocommerce_not_active_notice()
+{
+    echo '<div class="notice notice-error"><p>';
+    _e('Плагин требует активный WooCommerce для работы.', 'delivery-mne');
+    echo '</p></div>';
+}
---
+++ plugins/delivery-mne/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mne/delivery',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0b116525cb17e64ce98654f9835e07655453f39c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mne/delivery' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0b116525cb17e64ce98654f9835e07655453f39c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
