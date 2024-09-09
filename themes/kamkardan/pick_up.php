af7168b5aa0b2a2f6d449abfb1843e97bd17fd4d
Mariia Beliakova <mari.mv2008@gmail.com>
2024-09-09 21:22:58 +0200

    page pick up

--- themes/kamkardan/pick_up.php
+++ themes/kamkardan/pick_up.php
@@ -7,51 +7,50 @@ $fields = get_fields();
 ?>
 
 <main id="primary" class="site-page">
-		<div class="wrap">
-            <h2 class="wp-block-heading has-text-align-center margin-bottom-24"><?= $fields['zagolovok']; ?></h2>
-            <h3 class="wp-block-heading has-text-align-center margin-bottom-24"><?= $fields['podzagolovok']; ?></h3>
-			<?php
-            //the_title( '<h2 class="entry-title">', '</h2>' );
-			// while ( have_posts() ) :
-			// 	the_post();
+    <div class="wrap">
+        <h2 class="wp-block-heading has-text-align-center margin-bottom-24"><?= $fields['zagolovok']; ?></h2>
+        <h3 class="wp-block-heading has-text-align-center margin-bottom-24"><?= $fields['podzagolovok']; ?></h3>
+        <?php
+        //the_title( '<h2 class="entry-title">', '</h2>' );
+        // while ( have_posts() ) :
+        // 	the_post();
 
-			// 	get_template_part( 'template-parts/content', 'page' );
+        // 	get_template_part( 'template-parts/content', 'page' );
 
-			// endwhile; // End of the loop.
-			?>
+        // endwhile; // End of the loop.
+        ?>
 
-                <?php
-                // Получаем текущий ID страницы
-                $parent_id = get_the_ID();
+            <?php
+            // Получаем текущий ID страницы
+            $parent_id = get_the_ID();
 
-                // Параметры для получения дочерних страниц
-                $args = array(
-                    'post_type'      => 'page',
-                    'parent'    => $parent_id,
-                    'post_status'  => 'publish',
-                );
+            // Параметры для получения дочерних страниц
+            $args = array(
+                'post_type'      => 'page',
+                'parent'    => $parent_id,
+                'post_status'  => 'publish',
+            );
 
-                // Получаем дочерние страницы
-                $child_pages = get_pages($args);
+            // Получаем дочерние страницы
+            $child_pages = get_pages($args);
 
-                if (!empty($child_pages)) {
-                    echo '<div class="steps bottom">';
+            if (!empty($child_pages)) {
+                echo '<div class="steps bottom">';
 
-                    foreach ($child_pages as $itm) {
-                        $GLOBALS['post'] = $itm;
-                        setup_postdata($itm);
-                        get_template_part( 'template-parts/preview', 'pick-up' );
-                    }
-                    wp_reset_postdata();
-                    echo '</div>';
-                } else {
-                    echo 'У этой страницы нет дочерних страниц.';
+                foreach ($child_pages as $itm) {
+                    $GLOBALS['post'] = $itm;
+                    setup_postdata($itm);
+                    get_template_part( 'template-parts/preview', 'pick-up' );
                 }
-                ?>
-
-		</div>
-		<?php the_content(); ?>
-	</main><!-- #main -->
+                wp_reset_postdata();
+                echo '</div>';
+            } else {
+                echo 'У этой страницы нет дочерних страниц.';
+            }
+            ?>
+            <?php the_content(); ?>
+    </div>	
+</main><!-- #main -->
 
 <?php
 get_sidebar();
